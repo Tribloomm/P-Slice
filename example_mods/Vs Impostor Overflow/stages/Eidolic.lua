502a5d04c10a00f1423fdfc3cfd78e8edbf04444
Tribloom <136417464+Tribloomm@users.noreply.github.com>
2024-11-27 19:28:56 -0600

    background tweaks for cyan
    they're all done now yippie

--- example_mods/Vs Impostor Overflow/stages/Eidolic.lua
+++ example_mods/Vs Impostor Overflow/stages/Eidolic.lua
@@ -19,23 +19,24 @@ function onCreate()
 		scaleObject('EG', 0.9, 0.9);
 
 		makeLuaSprite('EF', 'BG/Electrical/EF', -500, -300);
-		setLuaSpriteScrollFactor('EF', 0.95, 1);
+		setLuaSpriteScrollFactor('EF', 0.9, 1);
 		scaleObject('EF', 0.9, 0.9);
 
-		makeLuaSprite('EBG', 'BG/Electrical/EBG', -935, -250);
-		setLuaSpriteScrollFactor('EBG', 1, 1);
-		scaleObject('EBG', 0.7, 0.7);
+		makeLuaSprite('EBG', 'BG/Electrical/EBG', -500, -300);
+		setLuaSpriteScrollFactor('EBG', 0.8, 1);
+		scaleObject('EBG', 0.9, 0.9);
 
-		makeLuaSprite('star', 'BG/Electrical/star', -500, -300);
-		setLuaSpriteScrollFactor('star', 1, 1);
-		scaleObject('star', 0.9, 0.9);
+		makeLuaSprite('front', 'BG/Electrical/front', -500, -300);
+		setLuaSpriteScrollFactor('front', 1, 1);
+		setBlendMode('front','add')
+		scaleObject('front', 0.9, 0.9);
 	end
 
-	addLuaSprite('star', false);
 	addLuaSprite('EBG', false);
 	addLuaSprite('EF', false);
 	addLuaSprite('EG', false);
 
+	addLuaSprite('front', true);
 	addLuaSprite('snow', true)
 
 	close(true);
